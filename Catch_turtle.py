4e20bd2da2c94ab3e29ec924360b2f9b15763583
Furkan Kaan Alkan <alkanfurkankaan@gmail.com>
2025-12-07 22:47:12 +0300

    counter is added beng a command line

--- Catch_turtle.py
+++ Catch_turtle.py
@@ -35,3 +35,29 @@ screen.listen()
 
 
 screen.mainloop()
+
+"""
+import time
+import sys
+from plyer import notifi
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = f"{mins:02d}:{secs:02d}"
+
+        sys.stdout.write("\r" + timer + "   ")
+        sys.stdout.flush()
+
+        time.sleep(1)
+        t -= 1
+
+    print("\nZamanlayıcı sona erdi")
+
+    notification.notify(
+        title="Time manager",
+        message="TİME İS OUT !!!",
+        timeout=1
+    )
+t = int(input("zamanı giriniz: "))
+countdown(t)2
+"""
